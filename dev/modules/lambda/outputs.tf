2b760c996dc0e2fe43f88daf75eb34626d9c093e
yisakm9 <192582247+yisakm9@users.noreply.github.com>
2025-10-17 09:41:26 +0300

    update output references to align with refactored resource name

--- dev/modules/lambda/outputs.tf
+++ dev/modules/lambda/outputs.tf
@@ -2,10 +2,12 @@
 
 output "function_arn" {
   description = "The ARN of the Lambda function."
-  value       = aws_lambda_function.cost_report_lambda.arn
+  # --- USE THE CORRECT RESOURCE NAME 'this' ---
+  value       = aws_lambda_function.this.arn
 }
 
 output "function_name" {
   description = "The name of the Lambda function."
-  value       = aws_lambda_function.cost_report_lambda.function_name
+  # --- USE THE CORRECT RESOURCE NAME 'this' ---
+  value       = aws_lambda_function.this.function_name
 }
